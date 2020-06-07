9bfee0262c29e3ec03bb40747adb0f3f68b0ddff
Ammon Jeffery <savingphace@gmail.com>
2020-06-06 20:31:38 -0600

    Testing git

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
- 
+ // initial variables
+var charArray = "";
+  const upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  const lowCase = "abcdefghijklmnopqrstuvwxyz".split('');
+  const numbers ="0123456789".split('') ;
+  const specChar = "!@#$%^&*(){}><?".split('');
 
-  const upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowCase = "abcdefghijklmnopqrstuvwxyz";
-  const numbers ="0123456789" ;
-  const specChar = "!@#$%^&*(){}><?";
+  let useUpCase = false;
+  let useLowCase = false;
+  let useNumbers = false;
+  let useSpecChar = false;
 
- var passLength = prompt = "Pick the number of characters you would like in your password between 8 and 128.";
+ var passLength = prompt("Pick the number of characters you would like in your password between 8 and 128.");
 
- if (passLength < 8 || [passLength >128]) {
+ if (passLength < 8 || passLength >128) {
    alert = "Please pick a number within the specified range."
-   var passLength = prompt = "Pick the number of characters you would like in your password between 8 and 128.";
+   var passLength = prompt("Pick the number of characters you would like in your password between 8 and 128.");
  }
- var yesUpCase = confirm ="Would you like to have upper case characters?";
+
+
+ var useUpCase = confirm("Would you like to have upper case characters?");
  
  
  if (yesUpCase === true){
 
  }
  var i;
-for (i=0; i<passLength; i++) {
 
+
+ for (i=0; i<passLength; i++) {
+password = password + charArray[Math.floor(Math.random() * charArray.length)]
 }
  
 // Write password to the #password input
